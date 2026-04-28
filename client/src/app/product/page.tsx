934bf5f46852f5f682788d28f3319def6994706d
shahzadhassan1977 <48802391+shahzadhassan1977@users.noreply.github.com>
2026-04-29 00:53:30 +0500

    product management add

--- client/src/app/product/page.tsx
+++ client/src/app/product/page.tsx
@@ -1,8 +1,44 @@
+"use client";
+
+import { useState } from "react";
+import PageWrapper from "@/components/ui/PageWrapper";
+import ProductTable from "@/modules/product/components/ProductTable";
+import ProductFormModal from "@/modules/product/components/ProductFormModal";
+
 export default function ProductPage() {
+  const [open, setOpen] = useState(false);
+  const [editData, setEditData] = useState<any>(null);
+
   return (
-    <div>
-      <h1 className="text-2xl font-bold">Product</h1>
-      <p>Manage ISP Product here</p>
-    </div>
+    <PageWrapper
+      title="Product Management"
+      description="Manage ISP product details"
+      action={
+        <button
+          onClick={() => {
+            setEditData(null);
+            setOpen(true);
+          }}
+          className="bg-blue-600 text-white px-4 py-2 rounded-lg"
+        >
+          + Add Product
+        </button>
+      }
+    >
+      <ProductTable
+        onEdit={(row: any) => {
+          setEditData(row);
+          setOpen(true);
+        }}
+      />
+
+      {open && (
+        <ProductFormModal
+          open={open}
+          data={editData}
+          onClose={() => setOpen(false)}
+        />
+      )}
+    </PageWrapper>
   );
 }
---
+++ server/dist/auth/entities/auth-otp.entity.d.ts
@@ -0,0 +1,9 @@
+export declare class AuthOTP {
+    authotpId: number;
+    email: string;
+    code: string;
+    isUsed: boolean;
+    expiresAt: Date;
+    createdAt: Date;
+    updatedAt: Date;
+}
